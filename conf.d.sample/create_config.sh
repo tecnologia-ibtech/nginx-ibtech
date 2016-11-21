2b61236b47cc7de89a7682abaf0acaaec108a9ce
Luis Fernando Murara <murara@Mac-do-Murara.local>
2016-11-21 17:50:52 -0200

    corrigida configuracao via shell script

--- conf.d.sample/create_config.sh
+++ conf.d.sample/create_config.sh
@@ -4,6 +4,6 @@ curl -o $1/10-Otimizacao.conf https://raw.githubusercontent.com/ibtechitsolution
 curl -o $1/20-Default.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/20-Default.conf
 curl -o $1/50-Couchdb.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/50-Couchdb.conf
 
-sed -i 's/\$REMOTE_HOST/'$2'/g' $1/*
-sed -i 's/\$PROXY_TOMCAT/'$3'/g' $1/*
-sed -i 's/\$PROXY_COUCHDB/'$4'/g' $1/*
+/usr/bin/sed -i 's/\$REMOTE_HOST/'$2'/g' $1/*
+/usr/bin/sed -i 's/\$PROXY_TOMCAT/'$3'/g' $1/*
+/usr/bin/sed -i 's/\$PROXY_COUCHDB/'$4'/g' $1/*
