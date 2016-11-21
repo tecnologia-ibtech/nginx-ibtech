96b80320aab00eedcd52abfeef7c7d74f4da3029
Luis Fernando Murara <murara@Mac-do-Murara.local>
2016-11-21 17:54:47 -0200

    corrigida configuracao via shell script

--- conf.d.sample/create_config.sh
+++ conf.d.sample/create_config.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-curl -o $1/10-Otimizacao.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/10-Otimizacao.conf
-curl -o $1/20-Default.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/20-Default.conf
-curl -o $1/50-Couchdb.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/50-Couchdb.conf
+curl -o $DIR/10-Otimizacao.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/10-Otimizacao.conf
+curl -o $DIR/20-Default.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/20-Default.conf
+curl -o $DIR/50-Couchdb.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/50-Couchdb.conf
 
-/usr/bin/sed -i 's/\$REMOTE_HOST/'$2'/g' $1/*
-/usr/bin/sed -i 's/\$PROXY_TOMCAT/'$3'/g' $1/*
-/usr/bin/sed -i 's/\$PROXY_COUCHDB/'$4'/g' $1/*
+/usr/bin/sed -i 's/\$REMOTE_HOST/'$REMOTE_HOST'/g' $DIR/*
+/usr/bin/sed -i 's/\$PROXY_TOMCAT/'$PROXY_TOMCAT'/g' $DIR/*
+/usr/bin/sed -i 's/\$PROXY_COUCHDB/'$PROXY_COUCHDB'/g' $DIR/*
