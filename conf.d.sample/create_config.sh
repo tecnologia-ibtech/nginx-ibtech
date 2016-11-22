7151fa58a53360fe39221ed45d04ccf8ed26109b
Luis Fernando Murara <murara@Mac-do-Murara.local>
2016-11-22 10:18:37 -0200

    Adicionado expires no script de configuracao

--- conf.d.sample/create_config.sh
+++ conf.d.sample/create_config.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-curl -o $DIR/10-Otimizacao.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/10-Otimizacao.conf
-curl -o $DIR/20-Default.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/20-Default.conf
-curl -o $DIR/50-Couchdb.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/50-Couchdb.conf
+curl -o $DIR/includes/expires.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/includes/expires.conf
+curl -o $DIR/conf.d/10-Otimizacao.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/10-Otimizacao.conf
+curl -o $DIR/conf.d/20-Default.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/20-Default.conf
+curl -o $DIR/conf.d/50-Couchdb.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/50-Couchdb.conf
 
-/usr/bin/sed -i 's/\$REMOTE_HOST/'$REMOTE_HOST'/g' $DIR/*
-/usr/bin/sed -i 's/\$PROXY_TOMCAT/'$PROXY_TOMCAT'/g' $DIR/*
-/usr/bin/sed -i 's/\$PROXY_COUCHDB/'$PROXY_COUCHDB'/g' $DIR/*
+/usr/bin/sed -i 's/\$REMOTE_HOST/'$REMOTE_HOST'/g' $DIR/conf.d/*
+/usr/bin/sed -i 's/\$PROXY_TOMCAT/'$PROXY_TOMCAT'/g' $DIR/conf.d/*
+/usr/bin/sed -i 's/\$PROXY_COUCHDB/'$PROXY_COUCHDB'/g' $DIR/conf.d/*
