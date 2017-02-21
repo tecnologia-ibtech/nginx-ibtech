8ec0010c25019ffb7135c07f6bc4b46e98f1b55a
murara <murara@atontech.net>
2017-02-21 17:27:45 -0300

    Adicionado arquivo 15Proxy

--- conf.d.sample/create_config.sh
+++ conf.d.sample/create_config.sh
@@ -2,6 +2,7 @@
 
 curl -o $DIR/includes/expires.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/includes/expires.conf
 curl -o $DIR/conf.d/10-Otimizacao.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/10-Otimizacao.conf
+curl -o $DIR/conf.d/15-Proxy.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/15-Proxy.conf
 curl -o $DIR/conf.d/20-Default.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/20-Default.conf
 curl -o $DIR/conf.d/50-Couchdb.conf https://raw.githubusercontent.com/ibtechitsolutions/docker-lua-resty-auto-ssl/master/conf.d.sample/50-Couchdb.conf
 
